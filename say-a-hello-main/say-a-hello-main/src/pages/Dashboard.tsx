cfd3159936bcc9a629c32dd9e1ae9027dc1576bd
Mahmoud-123Makhaly <mahmoudmakhaly123@gmail.com>
2025-10-26 02:44:48 -0800

    -

--- say-a-hello-main/say-a-hello-main/src/pages/Dashboard.tsx
+++ say-a-hello-main/say-a-hello-main/src/pages/Dashboard.tsx
@@ -445,7 +445,7 @@ const Dashboard = forwardRef((props: DashboardProps, ref) => {
   }
 
   return (
-    <div className="flex h-[700px] overflow-hidden w-screen bg-background">
+    <div className="flex overflow-hidden  bg-background">
       <div className="flex h-full w-full flex-col">
         {/* Mobile menu button */}
         <div className="lg:hidden flex items-center justify-between p-4 border-b bg-background">
